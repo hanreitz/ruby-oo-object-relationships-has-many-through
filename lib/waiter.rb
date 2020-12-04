6d5f3d4cb84a6fef328d0872629d5014d8333144
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-12-04 17:50:02 -0500

    commit tree ef2ca106bf42da89a1c8590e340f41afe4dfbbf4

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -32,4 +32,34 @@ class Waiter
         end
         best_tipped_meal.customer
     end 
+
+    # def most_frequent_customer
+    #     customers.max_by {|customer| customers.count(customer)}
+    # end
+
+    # def worst_tipped_meal
+    #     worst_tipped = meals.min {|meal_a, meal_b| meal_a.tip <=> meal_b.tip}
+    #     worst_tipped
+    # end
+
+    # def self.most_experienced_waiter
+    #     waiter_experience = {}
+    #     Meal.all.each {|meal| waiter_experience[meal.waiter] += 1}
+    #     waiter_experience.max_by {|waiter, meals| meals}[0]
+    # end
+
+    # def self.least_experienced_waiter
+    #     waiter_experience = {}
+    #     Meal.all.each {|meal| waiter_experience[meal.waiter] += 1}
+    #     waiter_experience.min_by {|waiter, meals| meals}[0]
+    # end
+
+    # def average_tips
+    #     tips = meals.collect {|meal| meal.tip}
+    #     average_tips = tips.reduce(+:)/tips.size.to_f
+    #     average_tips
+    # end
+
+    # puts Waiter.most_experienced_waiter.average_tips
+        
 end
